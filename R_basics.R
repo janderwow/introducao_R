e5f34772bb7bd4e0f06b79779a88e10b234944b2
Jander Melo <janderwow@gmail.com>
2023-10-30 20:23:28 -0300

    initial commit

---
+++ R_basics.R
@@ -0,0 +1,11 @@
+# imprimindo em R
+print("Coding in R")
+
+# para trazer informacoes sobre a funcao
+?print
+
+# criando comentarios em R
+
+# criando variaveis em R
+
+first_variable <- "This is my variable"
