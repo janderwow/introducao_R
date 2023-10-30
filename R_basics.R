5d89a10252c826e29c01e5e72eda4959269e196d
Jander Melo <janderwow@gmail.com>
2023-10-30 20:33:28 -0300

    entendendo estruturas basicas

--- R_basics.R
+++ R_basics.R
@@ -1,11 +1,21 @@
 # imprimindo em R
 print("Coding in R")
 
+# criando comentarios em R
+
 # para trazer informacoes sobre a funcao
 ?print
 
-# criando comentarios em R
-
 # criando variaveis em R
-
 first_variable <- "This is my variable"
+
+# variavel com formato numerico
+second_variable <- 12.5
+
+# criando um vetor
+vec_1 <- c(13, 48.5, 71, 101.5, 2)
+vec_1
+
+# representacao de pipes
+# %>%
+
