91bc53dde3e448a59db5631b1b9a11400e709825
janderwow <janderwow@gmail.com>
2023-12-18 11:19:37 -0300

    introducao a if/elseif/else

---
+++ R_if_else.R
@@ -0,0 +1,24 @@
+#instrucoes condicionais em R
+#if
+x <- 4
+if(x>0){
+  print("x é um número positivo")
+}
+
+#else
+y <- -3
+if(y > 0){
+  print("y é um número positivo")
+}else{
+  print("y é um número negativo")
+}
+
+#else if
+z <- 
+if(z<0){
+  print("z é negativo")
+}else if(z == 0){
+  print("z é igual a 0")
+}else{
+  print("z é positivo")
+}
