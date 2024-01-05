2df371f72816de1395dcbe9047e165d837fbd83e
janderwow <janderwow@gmail.com>
2024-01-05 10:51:06 -0300

    trabalhando com pipes

---
+++ R_pipes.R
@@ -0,0 +1,21 @@
+# lendo dataset
+data("ToothGrowth")
+
+# vizualizando dados em uma nova aba
+View(ToothGrowth)
+
+# instalando pacote dplyr
+install.packages('dplyr')
+library(dplyr)
+
+filtered_tg <- filter(ToothGrowth, dose == 0.5)
+View(filtered_tg)
+
+arrange(filtered_tg, len)
+
+arrange(filter(ToothGrowth, dose == 0.5), len)
+
+filtered_toothgrowth <- ToothGrowth %>% 
+  filter(dose==0.5) %>% 
+  group_by(supp) %>% 
+  summarize(mean_len = mean(len,na.rm = T),.group="drop") %>% 
