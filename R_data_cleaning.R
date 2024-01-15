b96214824db96899128f0dbd34225728b738ec22
janderwow <janderwow@gmail.com>
2024-01-15 09:08:20 -0300

    data cleaning

---
+++ R_data_cleaning.R
@@ -0,0 +1,11 @@
+# data cleaning
+install.packages("diplyr")
+
+install.packages("here") # facilita a referencia dos arquivos
+install.packages("skimr") # resumo dos dados com facilidade
+install.packages("janitor") #contem funcoes para data cleaning
+library("diplyr")
+library("here")
+library("skimr")
+library("janitor")
+
