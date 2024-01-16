2b79955ec743c9433ceafc3421d2154fb8cd0f17
janderwow <janderwow@gmail.com>
2024-01-16 11:11:44 -0300

    trabalhando com limpeza de dados

--- R_data_cleaning.R
+++ R_data_cleaning.R
@@ -1,11 +1,31 @@
 # data cleaning
-install.packages("diplyr")
-
+install.packages("dplyr") # data manipulation
 install.packages("here") # facilita a referencia dos arquivos
 install.packages("skimr") # resumo dos dados com facilidade
 install.packages("janitor") #contem funcoes para data cleaning
-library("diplyr")
+install.packages("palmerpenguins")
+library("dplyr")
 library("here")
 library("skimr")
 library("janitor")
+library("palmerpenguins")
 
+# resumos do df
+skim_without_charts(penguins) # sumario do df
+glimpse(penguins)
+head(penguins)
+
+# selecionando colunas
+penguins %>%
+  select(species) # "-species" tras o contrário
+
+# renomeando colunas
+penguins %>%
+  rename(islan_new = island)
+
+# mudando nome com condicoes
+penguins %>%
+  rename_with(penguins, to uppper)
+
+# garante que tenha somente caracteres, numeros e _ nos nomes
+clean_names(penguins)
