e982da3dadc03431deaad762a1f55c23cdc0c35c
Jander Melo <janderwow@gmail.com>
2024-01-22 00:39:55 -0300

    quarteto de ascombe e data viz

---
+++ R_quarteto_de_ascombe.R
@@ -0,0 +1,13 @@
+install.packages('Tmisc')
+library(Tmisc)
+data(quartet)
+View(quartet)
+
+quartet %>%
+  group_by(set) %>%
+  summarise(mean(x), sd(x), mean(y), sd(y), cor(x,y))
+
+ggplot(quartet, aes(x,y)) + geom_point() + geom_smooth(method=lm, se=FALSE) + facet_wrap(~set)
+
+install.packages('datasauRus')
+library('datasauRus')
