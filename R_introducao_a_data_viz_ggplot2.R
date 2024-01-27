545a5c139a9cac9fd3e28632954afd49db7c454b
Jander Melo <janderwow@gmail.com>
2024-01-27 12:58:32 -0300

    estetica em data viz

--- R_introducao_a_data_viz_ggplot2.R
+++ R_introducao_a_data_viz_ggplot2.R
@@ -8,3 +8,19 @@ data("penguins")
 View(penguins)
 
 ggplot(data=penguins)+geom_point(mapping = aes(x=flipper_length_mm, y= body_mass_g))
+
+# mapeando variavel "species" com estetica de cor
+ggplot(data=penguins)+geom_point(mapping = aes(x=flipper_length_mm, y= body_mass_g, color =species))
+
+# mapeando variavel "species" com estetica de forma
+ggplot(data=penguins)+geom_point(mapping = aes(x=flipper_length_mm, y= body_mass_g, shape =species))
+
+# combinando mapeamento
+ggplot(data=penguins)+
+  geom_point(mapping = aes(x=flipper_length_mm, y= body_mass_g, color=species, shape=species, size=species))
+
+# utilizando "alpha" para densidade dos pontos do gráfico
+ggplot(data=penguins)+geom_point(mapping = aes(x=flipper_length_mm, y= body_mass_g, alpha = species, color = "purple"))
+
+# mudando aparencia com variaveis especificas, utilizar fora da função "aes"
+ggplot(data=penguins)+geom_point(mapping = aes(x=flipper_length_mm, y= body_mass_g, alpha = species),color = "purple")
