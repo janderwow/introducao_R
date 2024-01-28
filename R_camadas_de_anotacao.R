dcb86e268f928b5bb90cc79d9e895ff0b8909e92
Jander Melo <janderwow@gmail.com>
2024-01-27 23:25:44 -0300

    intro a anotacoes e salvamento de imagens

---
+++ R_camadas_de_anotacao.R
@@ -0,0 +1,20 @@
+library(readr)
+library(palmerpenguins)
+
+
+ggplot(data=penguins)+
+  geom_point(mapping = aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+  labs(title="Palmer Penguins: Body Mass vs Flipper Length", 
+        subtitle = "Sample of Three Penguin Species",
+        caption = "Data collected by Dr. kristen Gorman")+
+  annotate("text", x=220, y=3500, label="The Gentoo are the largest", fontface="bold", size=4.5, angle=25)
+
+# o codigo pode ser incluido em uma variavel
+p <- ggplot(data=penguins)+
+  geom_point(mapping = aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+  labs(title="Palmer Penguins: Body Mass vs Flipper Length", 
+       subtitle = "Sample of Three Penguin Species",
+       caption = "Data collected by Dr. kristen Gorman")
+
+  p + annotate("text", x=220, y=3500, label="The Gentoo are the largest", fontface="bold", size=4.5, angle=25)
+  
